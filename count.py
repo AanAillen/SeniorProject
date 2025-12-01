25c1406797111037e51dce4e22fd92a35bbc2a6d
Tyler Cowles <146116000+tcowle@users.noreply.github.com>
2025-12-01 15:06:55 -0500

    add counting script

---
+++ count.py
@@ -0,0 +1,55 @@
+import cv2
+
+from ultralytics import solutions
+
+cap = cv2.VideoCapture('testVideos/youtube_fish.mov')
+assert cap.isOpened(), "Error reading video file"
+
+# Video writer
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+# Calculate vertical box in center of video
+box_width = 10  # width of the vertical box in pixels
+box_top = 0  # start 10% from top
+box_bottom = int(h)  # end 10% from bottom
+center_x = w // 2
+left_x = center_x - box_width // 2 - 100
+right_x = center_x + box_width // 2 - 100
+
+# Vertical rectangular region (clockwise: top-left, top-right, bottom-right, bottom-left)
+region_points = [
+    (left_x, box_top),      # top-left
+    (left_x, box_bottom)    # bottom-left
+]
+
+# region_points = [(20, 400), (1080, 400)]                                      # line counting
+# region_points = [(20, 400), (1080, 400), (1080, 360), (20, 360)]  # rectangular region
+# region_points = [(20, 400), (1080, 400), (1080, 360), (20, 360), (20, 400)]   # polygon region
+video_writer = cv2.VideoWriter("object_counting_output1.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+
+# Initialize object counter object
+counter = solutions.ObjectCounter(
+    show=True,  # display the output
+    region=region_points,  # pass region points
+    model='cfd-yolov12x-1.00.pt',  # model="yolo11n-obb.pt" for object counting with OBB model.
+    # classes=[0, 2],  # count specific classes, i.e., person and car with the COCO pretrained model.
+    # tracker="botsort.yaml",  # choose trackers i.e "bytetrack.yaml"
+)
+
+# Process video
+while cap.isOpened():
+    success, im0 = cap.read()
+
+    if not success:
+        print("Video frame is empty or processing is complete.")
+        break
+
+    results = counter(im0)
+
+    # print(results)  # access the output
+
+    video_writer.write(results.plot_im)  # write the processed frame.
+
+cap.release()
+video_writer.release()
+cv2.destroyAllWindows()  # destroy all opened windows
